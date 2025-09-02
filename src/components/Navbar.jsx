3b0c41ffbe68a31e1bb411c945d3cf768d8bf7d1
matias-05 <matifer344@gmail.com>
2025-09-02 15:20:26 -0300

    version primer entrega

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
       <img src={logoBudin} alt="Logo Budin" className='logo-budin'/>
       <h1 className='titulo-empresa'>Rincón del Budín</h1>
         <ul>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/productos">Productos</a></li>
-          <li><a href="/quienes-somos">Quienes somos</a></li>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Quienes somos</a></li>
         </ul>
       <CartWidget />
     </header>
