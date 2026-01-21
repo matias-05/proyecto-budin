2d78cc6a9e2d0673db3e779dada92cd4bc31a92e
matias-05 <matifer344@gmail.com>
2026-01-20 21:22:37 -0300

    cambio de imagenes en media

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,27 +1,30 @@
-import carrito from '../../media/logo-carrito.png';
-import { useContext } from 'react';
-import { cartContext } from '../../context/cartContext';
-import { Link } from 'react-router-dom';
+import carrito from "../../media/logo-carrito.webp";
+import { useContext } from "react";
+import { cartContext } from "../../context/cartContext";
+import { Link } from "react-router-dom";
 
 export default function CartWidget() {
   const { countCartItems } = useContext(cartContext);
   const totalItems = countCartItems();
 
   return (
-    <Link to="/carrito" className="flex items-center group transition-all duration-300">
-      
+    <Link
+      to="/carrito"
+      className="flex items-center group transition-all duration-300"
+    >
       {/* CONTENEDOR DEL ICONO: 
           order-1 en mobile (izquierda) 
           md:order-2 en desktop (derecha) */}
       <div className="relative flex items-center justify-center cursor-pointer order-1 md:order-2">
-        <img 
-          src={carrito} 
-          alt="Cart" 
-          className="h-8 w-8 object-contain transition-transform duration-300 group-hover:scale-110 group-hover:-rotate-12" 
+        <img
+          src={carrito}
+          alt="Cart"
+          className="h-8 w-8 object-contain transition-transform duration-300 group-hover:scale-110 group-hover:-rotate-12"
         />
 
         {totalItems > 0 && (
-          <span className="
+          <span
+            className="
             absolute -top-2 -right-2 
             bg-[#681104] text-[#e37b00] 
             text-[10px] font-black 
@@ -31,7 +34,8 @@ export default function CartWidget() {
             border border-[#e37b00]/30 
             shadow-lg 
             animate-in zoom-in duration-300
-          ">
+          "
+          >
             {totalItems}
           </span>
         )}
@@ -41,7 +45,6 @@ export default function CartWidget() {
           order-2 en mobile (derecha del carrito) 
           md:order-1 en desktop (izquierda del carrito) */}
       <div className="h-8 w-[2px] bg-[#681104] mx-4 md:mx-6 opacity-40 group-hover:opacity-100 transition-opacity order-2 md:order-1" />
-
     </Link>
   );
 }
